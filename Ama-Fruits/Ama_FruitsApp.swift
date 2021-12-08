16d9b4060ab290dfb12a64c85af6fc55aa0dfef1
Samuel Noye <samuel.noye@amalitech.org>
2021-12-08 15:02:05 +0000

    Initial Commit

---
+++ Ama-Fruits/Ama_FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ama_FruitsApp.swift
+//  Ama-Fruits
+//
+//  Created by Samuel Noye on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Ama_FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
