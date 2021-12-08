c612488e0d501407a095a7aa3f0a331015de8114
Samuel Noye <samuel.noye@amalitech.org>
2021-12-08 17:30:05 +0000

    views

--- Ama-Fruits/Views/FruitsCardView.swift
+++ Ama-Fruits/Views/FruitsCardView.swift
@@ -13,11 +13,16 @@ struct FruitsCardView: View {
     
     //MARK: - BODY
     var body: some View {
-        VStack(spacing: 20){
+        ZStack{
             
-            //Fruits: Image
+            VStack(spacing: 20){
+                //Fruits: Image
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green:0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+            }
             //Fruits: Title
-            
             Text("Blueberry")
             //Fruits: Headline
             //Button: Start
