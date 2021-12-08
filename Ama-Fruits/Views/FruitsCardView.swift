3b59fb7238f2fce7463ca87ed3bd102094c4c3a9
Samuel Noye <samuel.noye@amalitech.org>
2021-12-08 15:47:19 +0000

    Start

---
+++ Ama-Fruits/Views/FruitsCardView.swift
@@ -0,0 +1,34 @@
+//
+//  FruitsCardView.swift
+//  Ama-Fruits
+//
+//  Created by Samuel Noye on 08/12/2021.
+//
+
+import SwiftUI
+
+struct FruitsCardView: View {
+    
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        VStack(spacing: 20){
+            
+            //Fruits: Image
+            //Fruits: Title
+            
+            Text("Blueberry")
+            //Fruits: Headline
+            //Button: Start
+            
+        }//: ZSTACK
+    }
+}
+//MARK: - PREVIEW
+struct FruitsCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitsCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
