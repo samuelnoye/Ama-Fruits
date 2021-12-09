743f5ea4c835bb241e3105057f0ab6169f1ef6ed
Samuel Noye <samuel.noye@amalitech.org>
2021-12-09 09:33:58 +0000

    Blueberry Animation

--- Ama-Fruits/Views/FruitsCardView.swift
+++ Ama-Fruits/Views/FruitsCardView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FruitsCardView: View {
     
     //MARK: - PROPERTIES
+    @State private var isAnimating: Bool = false
     
     //MARK: - BODY
     var body: some View {
@@ -28,6 +29,8 @@ struct FruitsCardView: View {
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                
             //Fruits: Headline
                 Text("Blueberriess are sweet, nutritious and wildly popular fruit all oer the world")
                     .foregroundColor(Color.white)
@@ -39,9 +42,14 @@ struct FruitsCardView: View {
                 StartButtonView()
             }//: VStack
         }//: ZSTACK
+        .onAppear{withAnimation(.easeOut(duration: 0.5)){
+            isAnimating = true
+         }
+        }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
         .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 //MARK: - PREVIEW
