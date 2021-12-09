900d0ec7af3c5918b1d18c38400e20f8af8afacf
Samuel Noye <samuel.noye@amalitech.org>
2021-12-09 09:21:13 +0000

    Start Button View

--- Ama-Fruits/Views/FruitsCardView.swift
+++ Ama-Fruits/Views/FruitsCardView.swift
@@ -21,13 +21,27 @@ struct FruitsCardView: View {
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green:0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
-            }
+            
             //Fruits: Title
             Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
             //Fruits: Headline
+                Text("Blueberriess are sweet, nutritious and wildly popular fruit all oer the world")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
             //Button: Start
-            
+                StartButtonView()
+            }//: VStack
         }//: ZSTACK
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 //MARK: - PREVIEW
---
+++ Ama-Fruits/Views/StartButtonView.swift
@@ -0,0 +1,45 @@
+//
+//  StartButtonView.swift
+//  Ama-Fruits
+//
+//  Created by Samuel Noye on 09/12/2021.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    
+    //MARK: - PROPERTIES
+    
+    
+    
+    //MARK: - BODY
+    
+    var body: some View {
+        Button(action:{
+            print("Exit the onboarding")
+        }){
+            HStack(spacing: 8){
+        Text("Start")
+        
+        Image(systemName: "arrow.right.circle")
+                .imageScale(.large)
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 10)
+        .background(
+            Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+        )
+    }
+        .accentColor(Color.white)
+}
+}
+
+    //MARK: - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(.dark)
+    }
+}
