b254aeb2b01d8954e1f75b9afc0d2b8f6471eae5
Samuel Noye <samuel.noye@amalitech.org>
2021-12-10 15:01:14 +0000

    Conditional layout

--- Ama-Fruits/Views/SettingsRowView.swift
+++ Ama-Fruits/Views/SettingsRowView.swift
@@ -10,14 +10,23 @@ import SwiftUI
 struct SettingsRowView: View {
     //MARK: - PROPERTIES
     var name: String
-    var content: String
+    var content: String? = nil
+    var linkLabel: String? = nil
+    var linkDestination: String? = nil
     
     //MARK: - BODY
     var body: some View {
         HStack{
             Text(name).foregroundColor(Color.gray)
             Spacer()
-            Text(content)
+            if (content !=  nil  ) {
+                Text(content!)
+            }else if(linkLabel != nil && linkDestination != nil){
+                Link(linkLabel!, destination: URL(string: "https//\(linkDestination!)")!)
+                Image(systemName: "arrow.up.arrow.sqare").foregroundColor(.pink)
+            } else {
+                EmptyView()
+            }
         }
     }
 }
