253a998c055b9815c8c84fbe2778b1ebbaaed05a
Samuel Noye <samuel.noye@amalitech.org>
2022-05-13 19:30:24 +0000

    Update ContentView.swift

--- Ama-Fruits/App/ContentView.swift
+++ Ama-Fruits/App/ContentView.swift
@@ -1,49 +1,49 @@
-// //
-// //  ContentView.swift
-// //  Ama-Fruits
-// //
-// //  Created by Samuel Noye on 08/12/2021.
-// //
+//
+//  ContentView.swift
+//  Ama-Fruits
+//
+//  Created by Samuel Noye on 08/12/2021.
+//
 
-// import SwiftUI
+import SwiftUI
 
-// struct ContentView: View {
+struct ContentView: View {
     
-//     //MARK: - PROPERTIES
-//     var fruits: [Fruit] = fruitData
-//     @State private var isShowSetting: Bool = false
+    //MARK: - PROPERTIES
+    var fruits: [Fruit] = fruitData
+    @State private var isShowSetting: Bool = false
      
-//     //MARK: - BODY
-//     var body: some View {
-//         NavigationView {
-//             List {
-//                 ForEach(fruits.shuffled()){ item in
-//                     NavigationLink(destination: FruitDetialView(fruit: item)){
-//                         FruitRowView(fruit: item)
-//                             .padding(.vertical, 4)
-//                     }
-//                 }
-//             }
-//             .navigationTitle("Fruits")
-//             .navigationBarItems(
-//                 trailing:
-//                  Button(action:{
-//                   isShowSetting = true
-//             }){
-//                 Image(systemName: "slider.horizontal.3")
-//               } //: BUTTON
-//                     .sheet(isPresented: $isShowSetting){
-//                         SettingView()
-//                     }
-//             )
-//         }//: NAVIGATION
-//         .navigationViewStyle(StackNavigationViewStyle())
-//     }
-// }
-//     //MARK: - PREVIEW
-// struct ContentView_Previews: PreviewProvider {
-//     static var previews: some View {
-//         ContentView(fruits: fruitData)
+    //MARK: - BODY
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()){ item in
+                    NavigationLink(destination: FruitDetialView(fruit: item)){
+                        FruitRowView(fruit: item)
+                            .padding(.vertical, 4)
+                    }
+                }
+            }
+            .navigationTitle("Fruits")
+            .navigationBarItems(
+                trailing:
+                 Button(action:{
+                  isShowSetting = true
+            }){
+                Image(systemName: "slider.horizontal.3")
+              } //: BUTTON
+                    .sheet(isPresented: $isShowSetting){
+                        SettingView()
+                    }
+            )
+        }//: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+    //MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(fruits: fruitData)
             
-//     }
-// }
+    }
+}
